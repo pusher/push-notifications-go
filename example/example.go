7bc31c8ed89d633d5c38962548b564d6294fa2f1
Luis Fonseca <luis@pusher.com>
2018-02-19 22:59:26 +0000

    Update example instance id and secret key

--- example/example.go
+++ example/example.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	instanceId = "8a070eaa-033f-46d6-bb90-f4c15acc47e1"
-	secretKey  = "A84891378A575A220BEB53352E84385"
+	instanceId = "96f84bc1-075f-4ac6-8a1b-eba0a54886f7"
+	secretKey  = "3B397552E080252048FE03009C1253A"
 )
 
 func main() {
